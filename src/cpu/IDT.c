6fdb269a0a5f5b09122d2a1da161a256099b8db6
jerryjhird <jerryjhird-public@proton.me>
2026-03-28 08:17:32 +0000

    finished adding all IRQ stubs

--- src/cpu/IDT.c
+++ src/cpu/IDT.c
@@ -178,8 +178,7 @@ void irq_drunk_handler(struct trap_frame *tf) {
     UNUSED(tf);
 }
 
-DEF_IRQ_HANDLER(32, irq_dispatch)
-DEF_IRQ_HANDLER(36, irq_dispatch)
+#include "IRQHL.h"
 
 DEF_IRQ_HANDLER(255, irq_drunk_handler)
 
@@ -195,9 +194,62 @@ static void (*interrupt_handlers[256])(void) = {
     [28] = handler_28, [29] = handler_29, [30] = handler_30, [31] = handler_31,
 
     // IRQs
-    [32] = irq_handler_32,
-    [36] = irq_handler_36,
-    [255] = irq_handler_255
+    [32] = irq_handler_32, [33] = irq_handler_33, [34] = irq_handler_34, [35] = irq_handler_35,
+    [36] = irq_handler_36, [37] = irq_handler_37, [38] = irq_handler_38, [39] = irq_handler_39,
+    [40] = irq_handler_40, [41] = irq_handler_41, [42] = irq_handler_42, [43] = irq_handler_43,
+    [44] = irq_handler_44, [45] = irq_handler_45, [46] = irq_handler_46, [47] = irq_handler_47,
+    [48] = irq_handler_48, [49] = irq_handler_49, [50] = irq_handler_50, [51] = irq_handler_51,
+    [52] = irq_handler_52, [53] = irq_handler_53, [54] = irq_handler_54, [55] = irq_handler_55,
+    [56] = irq_handler_56, [57] = irq_handler_57, [58] = irq_handler_58, [59] = irq_handler_59,
+    [60] = irq_handler_60, [61] = irq_handler_61, [62] = irq_handler_62, [63] = irq_handler_63,
+    [64] = irq_handler_64, [65] = irq_handler_65, [66] = irq_handler_66, [67] = irq_handler_67,
+    [68] = irq_handler_68, [69] = irq_handler_69, [70] = irq_handler_70, [71] = irq_handler_71,
+    [72] = irq_handler_72, [73] = irq_handler_73, [74] = irq_handler_74, [75] = irq_handler_75,
+    [76] = irq_handler_76, [77] = irq_handler_77, [78] = irq_handler_78, [79] = irq_handler_79,
+    [80] = irq_handler_80, [81] = irq_handler_81, [82] = irq_handler_82, [83] = irq_handler_83,
+    [84] = irq_handler_84, [85] = irq_handler_85, [86] = irq_handler_86, [87] = irq_handler_87,
+    [88] = irq_handler_88, [89] = irq_handler_89, [90] = irq_handler_90, [91] = irq_handler_91,
+    [92] = irq_handler_92, [93] = irq_handler_93, [94] = irq_handler_94, [95] = irq_handler_95,
+    [96] = irq_handler_96, [97] = irq_handler_97, [98] = irq_handler_98, [99] = irq_handler_99,
+    [100] = irq_handler_100, [101] = irq_handler_101, [102] = irq_handler_102, [103] = irq_handler_103,
+    [104] = irq_handler_104, [105] = irq_handler_105, [106] = irq_handler_106, [107] = irq_handler_107,
+    [108] = irq_handler_108, [109] = irq_handler_109, [110] = irq_handler_110, [111] = irq_handler_111,
+    [112] = irq_handler_112, [113] = irq_handler_113, [114] = irq_handler_114, [115] = irq_handler_115,
+    [116] = irq_handler_116, [117] = irq_handler_117, [118] = irq_handler_118, [119] = irq_handler_119,
+    [120] = irq_handler_120, [121] = irq_handler_121, [122] = irq_handler_122, [123] = irq_handler_123,
+    [124] = irq_handler_124, [125] = irq_handler_125, [126] = irq_handler_126, [127] = irq_handler_127,
+    [128] = irq_handler_128, [129] = irq_handler_129, [130] = irq_handler_130, [131] = irq_handler_131,
+    [132] = irq_handler_132, [133] = irq_handler_133, [134] = irq_handler_134, [135] = irq_handler_135,
+    [136] = irq_handler_136, [137] = irq_handler_137, [138] = irq_handler_138, [139] = irq_handler_139,
+    [140] = irq_handler_140, [141] = irq_handler_141, [142] = irq_handler_142, [143] = irq_handler_143,
+    [144] = irq_handler_144, [145] = irq_handler_145, [146] = irq_handler_146, [147] = irq_handler_147,
+    [148] = irq_handler_148, [149] = irq_handler_149, [150] = irq_handler_150, [151] = irq_handler_151,
+    [152] = irq_handler_152, [153] = irq_handler_153, [154] = irq_handler_154, [155] = irq_handler_155,
+    [156] = irq_handler_156, [157] = irq_handler_157, [158] = irq_handler_158, [159] = irq_handler_159,
+    [160] = irq_handler_160, [161] = irq_handler_161, [162] = irq_handler_162, [163] = irq_handler_163,
+    [164] = irq_handler_164, [165] = irq_handler_165, [166] = irq_handler_166, [167] = irq_handler_167,
+    [168] = irq_handler_168, [169] = irq_handler_169, [170] = irq_handler_170, [171] = irq_handler_171,
+    [172] = irq_handler_172, [173] = irq_handler_173, [174] = irq_handler_174, [175] = irq_handler_175,
+    [176] = irq_handler_176, [177] = irq_handler_177, [178] = irq_handler_178, [179] = irq_handler_179,
+    [180] = irq_handler_180, [181] = irq_handler_181, [182] = irq_handler_182, [183] = irq_handler_183,
+    [184] = irq_handler_184, [185] = irq_handler_185, [186] = irq_handler_186, [187] = irq_handler_187,
+    [188] = irq_handler_188, [189] = irq_handler_189, [190] = irq_handler_190, [191] = irq_handler_191,
+    [192] = irq_handler_192, [193] = irq_handler_193, [194] = irq_handler_194, [195] = irq_handler_195,
+    [196] = irq_handler_196, [197] = irq_handler_197, [198] = irq_handler_198, [199] = irq_handler_199,
+    [200] = irq_handler_200, [201] = irq_handler_201, [202] = irq_handler_202, [203] = irq_handler_203,
+    [204] = irq_handler_204, [205] = irq_handler_205, [206] = irq_handler_206, [207] = irq_handler_207,
+    [208] = irq_handler_208, [209] = irq_handler_209, [210] = irq_handler_210, [211] = irq_handler_211,
+    [212] = irq_handler_212, [213] = irq_handler_213, [214] = irq_handler_214, [215] = irq_handler_215,
+    [216] = irq_handler_216, [217] = irq_handler_217, [218] = irq_handler_218, [219] = irq_handler_219,
+    [220] = irq_handler_220, [221] = irq_handler_221, [222] = irq_handler_222, [223] = irq_handler_223,
+    [224] = irq_handler_224, [225] = irq_handler_225, [226] = irq_handler_226, [227] = irq_handler_227,
+    [228] = irq_handler_228, [229] = irq_handler_229, [230] = irq_handler_230, [231] = irq_handler_231,
+    [232] = irq_handler_232, [233] = irq_handler_233, [234] = irq_handler_234, [235] = irq_handler_235,
+    [236] = irq_handler_236, [237] = irq_handler_237, [238] = irq_handler_238, [239] = irq_handler_239,
+    [240] = irq_handler_240, [241] = irq_handler_241, [242] = irq_handler_242, [243] = irq_handler_243,
+    [244] = irq_handler_244, [245] = irq_handler_245, [246] = irq_handler_246, [247] = irq_handler_247,
+    [248] = irq_handler_248, [249] = irq_handler_249, [250] = irq_handler_250, [251] = irq_handler_251,
+    [252] = irq_handler_252, [253] = irq_handler_253, [254] = irq_handler_254, [255] = irq_handler_255
 };
 
 void idt_init(void) {
