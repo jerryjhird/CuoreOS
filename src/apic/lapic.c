16d8186ef5b27ffecaed306087d1035560b388e6
jerryjhird <jerryjhird-public@proton.me>
2026-04-03 12:13:49 +0100

    fixup! update lapic to use a generic read_msr call + new lapic functions for interacting with other cores

--- src/apic/lapic.c
+++ src/apic/lapic.c
@@ -8,52 +8,52 @@ void lapic_write(uint32_t reg, uint32_t data) {
 }
 
 uint32_t lapic_read(uint32_t reg) {
-    return *(volatile uint32_t*)(lapic_base + reg);
+	return *(volatile uint32_t*)(lapic_base + reg);
 }
 
 void lapic_wait_for_delivery(void) {
-    while (lapic_read(LAPIC_REG_ICR_LOW) & ICR_SEND_PENDING) {
-        __asm__ volatile ("pause");
-    }
+	while (lapic_read(LAPIC_REG_ICR_LOW) & ICR_SEND_PENDING) {
+		__asm__ volatile ("pause");
+	}
 }
 
 void lapic_send_ipi(uint8_t target_lapic_id, uint8_t vector) {
-    lapic_wait_for_delivery();
-    lapic_write(LAPIC_REG_ICR_HIGH, (uint32_t)target_lapic_id << 24);
-    lapic_write(LAPIC_REG_ICR_LOW, ICR_PHYSICAL | ICR_LEVEL_ASSERT | ICR_FIXED | vector);
+	lapic_wait_for_delivery();
+	lapic_write(LAPIC_REG_ICR_HIGH, (uint32_t)target_lapic_id << 24);
+	lapic_write(LAPIC_REG_ICR_LOW, ICR_PHYSICAL | ICR_LEVEL_ASSERT | ICR_FIXED | vector);
 }
 
 void lapic_send_init(uint8_t target_lapic_id) {
-    lapic_wait_for_delivery();
-    lapic_write(LAPIC_REG_ICR_HIGH, (uint32_t)target_lapic_id << 24);
+	lapic_wait_for_delivery();
+	lapic_write(LAPIC_REG_ICR_HIGH, (uint32_t)target_lapic_id << 24);
 
-    lapic_write(LAPIC_REG_ICR_LOW, ICR_INIT | ICR_PHYSICAL | ICR_LEVEL_ASSERT);
-    lapic_wait_for_delivery();
-    lapic_write(LAPIC_REG_ICR_LOW, ICR_INIT | ICR_PHYSICAL | ICR_LEVEL_DEASSERT);
+	lapic_write(LAPIC_REG_ICR_LOW, ICR_INIT | ICR_PHYSICAL | ICR_LEVEL_ASSERT);
+	lapic_wait_for_delivery();
+	lapic_write(LAPIC_REG_ICR_LOW, ICR_INIT | ICR_PHYSICAL | ICR_LEVEL_DEASSERT);
 }
 
 void lapic_send_sipi(uint8_t target_lapic_id, uintptr_t entry_point) {
-    lapic_wait_for_delivery();
-    lapic_write(LAPIC_REG_ICR_HIGH, (uint32_t)target_lapic_id << 24);
+	lapic_wait_for_delivery();
+	lapic_write(LAPIC_REG_ICR_HIGH, (uint32_t)target_lapic_id << 24);
 
-    uint8_t vector = (uint8_t)((entry_point >> 12) & 0xFF);
-    
-    lapic_write(LAPIC_REG_ICR_LOW, ICR_STARTUP | ICR_PHYSICAL | vector);
+	uint8_t vector = (uint8_t)((entry_point >> 12) & 0xFF);
+
+	lapic_write(LAPIC_REG_ICR_LOW, ICR_STARTUP | ICR_PHYSICAL | vector);
 }
 
 uint32_t lapic_get_id(void) {
-    return lapic_read(LAPIC_REG_ID) >> 24;
+	return lapic_read(LAPIC_REG_ID) >> 24;
 }
 
 void lapic_init(uintptr_t base_addr) {
 	lapic_base = base_addr;
 
-    uint64_t apic_base = read_msr(MSR_IA32_APIC_BASE);
+	uint64_t apic_base = read_msr(MSR_IA32_APIC_BASE);
 
-    if (!(apic_base & (1ULL << 11))) {
-        apic_base |= (1ULL << 11);
-        write_msr(MSR_IA32_APIC_BASE, apic_base);
-    }
+	if (!(apic_base & (1ULL << 11))) {
+		apic_base |= (1ULL << 11);
+		write_msr(MSR_IA32_APIC_BASE, apic_base);
+	}
 
 	lapic_write(LAPIC_REG_TPR, 0);
 	lapic_write(LAPIC_REG_SPURIOUS, 0x1FF);
