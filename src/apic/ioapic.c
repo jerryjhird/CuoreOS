7d5e92a3a15c134403c457c8f9c914e61631bbca
jerryjhird <jerryjhird-public@proton.me>
2026-04-05 09:03:08 +0100

    integrated madt into ioapic

--- src/apic/ioapic.c
+++ src/apic/ioapic.c
@@ -2,6 +2,8 @@
 #include <limine.h>
 #include "kstate.h"
 #include "ioapic.h"
+#include "logbuf.h"
+#include "madt.h"
 
 #define IOAPIC_REG_SEL  0x00
 #define IOAPIC_REG_WIN  0x10
@@ -13,12 +15,24 @@ static void ioapic_write(uint32_t reg, uint32_t value) {
 	*(volatile uint32_t*)(ioapic_virt_base + IOAPIC_REG_WIN) = value;
 }
 
+static uint32_t ioapic_read(uint32_t reg) {
+	*(volatile uint32_t*)(ioapic_virt_base + IOAPIC_REG_SEL) = reg;
+	return *(volatile uint32_t*)(ioapic_virt_base + IOAPIC_REG_WIN);
+}
+
 void ioapic_map_irq(uint8_t irq_pin, uint8_t vector, uint8_t cpu_apic_id, uint32_t flags) {
 	uint32_t low_index = 0x10 + (irq_pin * 2);
 	ioapic_write(low_index + 1, (uint32_t)cpu_apic_id << 24);
-
 	uint32_t low_entry = (uint32_t)vector | flags;
 	ioapic_write(low_index, low_entry);
+
+	logbuf_vwrite(LOG_LEVEL_DEBUG, "[IOAPIC] mapped pin: ");
+	logbuf_vputint(LOG_LEVEL_DEBUG, irq_pin);
+	logbuf_vwrite(LOG_LEVEL_DEBUG, ", vector:");
+	logbuf_vputint(LOG_LEVEL_DEBUG, vector);
+	logbuf_vwrite(LOG_LEVEL_DEBUG, ", flags:");
+	logbuf_vputint(LOG_LEVEL_DEBUG, flags);
+	logbuf_vwrite(LOG_LEVEL_DEBUG, "\n");
 }
 
 void ioapic_init(uintptr_t base_addr) {
@@ -27,6 +41,26 @@ void ioapic_init(uintptr_t base_addr) {
 	}
 
 	ioapic_virt_base = base_addr;
-	ioapic_map_irq(2, 32, 0, 0);
-	ioapic_map_irq(4, 36, 0, IOAPIC_TRIGGER_EDGE | IOAPIC_POLARITY_HIGH); // COM1 > vector 36 > cpu 0
+
+	uint32_t ver = ioapic_read(IOAPIC_REG_VER);
+	uint8_t max_pins = (uint8_t)((ver >> 16) & 0xFF);
+
+	for (uint8_t i = 0; i <= max_pins; i++) {
+		ioapic_map_irq(i, 0, 0, (1 << 16));
+	}
+
+	uint8_t pin;
+	uint32_t flags;
+
+	// Timer
+	madt_query_irq(0, &pin, &flags);
+	ioapic_map_irq(pin, 32, 0, flags);
+
+	// Keyboard
+	// madt_query_irq(1, &pin, &flags);
+	// ioapic_map_irq(pin, 33, 0, flags);
+
+	// COM1
+	madt_query_irq(4, &pin, &flags);
+	ioapic_map_irq(pin, 36, 0, flags);
 }
--- src/apic/ioapic.h
+++ src/apic/ioapic.h
@@ -2,11 +2,14 @@
 
 #include <stdint.h>
 
-#define IOAPIC_DELIVERY_FIXED  0x000
+#define IOAPIC_DELIVERY_FIXED 0x000
 #define IOAPIC_POLARITY_LOW	(1 << 13)
-#define IOAPIC_POLARITY_HIGH   (0 << 13)
-#define IOAPIC_TRIGGER_LEVEL   (1 << 15)
+#define IOAPIC_POLARITY_HIGH (0 << 13)
+#define IOAPIC_TRIGGER_LEVEL (1 << 15)
 #define IOAPIC_TRIGGER_EDGE	(0 << 15)
+#define IOAPIC_REG_VER 0x01
+#define IOAPIC_REG_SEL 0x00
+#define IOAPIC_REG_WIN 0x10
 
 void ioapic_init(uintptr_t base_addr);
 void ioapic_map_irq(uint8_t irq_pin, uint8_t vector, uint8_t cpu_apic_id, uint32_t flags);
