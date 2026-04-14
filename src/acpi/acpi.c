ce4cf6ea94b5306919e568188bb63317f04d7398
jerryjhird <jerryjhird-public@proton.me>
2026-04-14 18:46:43 +0100

    acpi: lay groundwork for BIOS booting support update acpi_init so it can handle systems lacking XSDT by falling back to RSDT pointer added 'is_xsdt' flag to distinguish between 32 bit and 64 bit system table pointers

--- src/acpi/acpi.c
+++ src/acpi/acpi.c
@@ -3,31 +3,40 @@
 #include "kstate.h"
 
 static struct acpi_sdt_header* xsdt = NULL;
+static bool is_xsdt = false;
 
 void acpi_init(void) {
 	struct limine_rsdp_response* resp = rsdp_request.response;
 	struct rsdp_v2* rsdp = (struct rsdp_v2*)resp->address;
 
-	if (rsdp->revision >= 2) {
+	if (rsdp->revision >= 2 && rsdp->xsdt_address != 0) {
 		xsdt = (struct acpi_sdt_header*)(rsdp->xsdt_address + hhdm_offset);
+		is_xsdt = true;
 	} else {
 		xsdt = (struct acpi_sdt_header*)((uint64_t)rsdp->rsdt_address + hhdm_offset);
+		is_xsdt = false;
 	}
 }
 
 void* acpi_find_sdt(const char* signature) {
 	if (!xsdt) return NULL;
 
-	int entries = (xsdt->length - sizeof(struct acpi_sdt_header)) / 8;
-	uint64_t* table_ptrs = (uint64_t*)((uint8_t*)xsdt + sizeof(struct acpi_sdt_header));
+	size_t ptr_size = is_xsdt ? 8 : 4;
+	int entries = (xsdt->length - sizeof(struct acpi_sdt_header)) / ptr_size;
+	uint8_t* table_ptrs_base = (uint8_t*)xsdt + sizeof(struct acpi_sdt_header);
 
 	for (int i = 0; i < entries; i++) {
-		struct acpi_sdt_header* table = (struct acpi_sdt_header*)(table_ptrs[i] + hhdm_offset);
+		uint64_t table_phys;
+		if (is_xsdt) {
+			table_phys = ((uint64_t*)table_ptrs_base)[i];
+		} else {
+			table_phys = ((uint32_t*)table_ptrs_base)[i];
+		}
 
+		struct acpi_sdt_header* table = (struct acpi_sdt_header*)(table_phys + hhdm_offset);
 		if (memcmp(table->signature, signature, 4) == 0) {
 			return (void*)table;
 		}
 	}
-
 	return NULL;
 }
