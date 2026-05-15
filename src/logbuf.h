354afe2a90f6b296b85ad4c0b69fade3c17e3c6a
jerryjhird <jerryjhird-public@proton.me>
2026-05-15 08:54:34 +0100

    stdio: implement generic callback based printf and trampoline logbuf_printf calls to it.

--- src/logbuf.h
+++ src/logbuf.h
@@ -17,36 +17,7 @@ void logbuf_putrawhex(uint64_t val);
 void logbuf_puthex64(uint64_t val);
 void logbuf_putint(uint64_t n);
 
-/**
- * flags:
- *  - # : adds 0x prefix for %x/%p, 0b for %b
- *  - 0 : zero padding
- *  - - : left alignment
- *  - + : prefaces positive numbers with +
- *  -   : prefaces positive numbers with a space.
- *
- * length modifiers:
- *  - hh  : argument is (unsigned) char.
- *  - h   : argument is (unsigned) short.
- *  - l   : argument is (unsigned) long.
- *  - ll  : argument is (unsigned) long long.
- *  - z   : argument is size_t.
- *  - j   : argument is intmax_t.
- *  - t   : argument is ptrdiff_t.
- *
- *  - d, i: signed decimal integer.
- *  - u : unsigned decimal integer.
- *  - x : unsigned hex
- *  - p : pointer.
- *  - s : null terminated str
- *  - c : single char
- *  - %
- *
- *  - b : binary representation
- *  - I : ipv4 addr (expects uint8_t[4])
- *  - M : mac addr (expects uint8_t[6])
- */
-void logbuf_printf(const char *fmt, ...);
+void logbuf_printf(const char *fmt, ...); // see formatting instructions in stdio.h
 
 void logbuf_flush(kernel_char_dev_t *target);
 void logbuf_clear(void);
--- src/stdio.h
+++ src/stdio.h
@@ -2,7 +2,40 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <stdarg.h>
 
 void ptrthex(char *buf, uint64_t val);
-void byteswap_str(char* str, size_t len);
 void readline(char (*getc_func)(void), void (*putc_func)(char), char* buffer, size_t limit);
+
+typedef void (*putc_t)(char c);
+
+/**
+ * flags:
+ *  - # : adds 0x prefix for %x, 0b for %b but %p always includes 0x
+ *  - 0 : zero padding
+ *  - - : left alignment
+ *  - + : prefaces positive numbers with +
+ *  -   : prefaces positive numbers with a space.
+ *
+ * length modifiers:
+ *  - hh  : argument is (unsigned) char.
+ *  - h   : argument is (unsigned) short.
+ *  - l   : argument is (unsigned) long.
+ *  - ll  : argument is (unsigned) long long.
+ *  - z   : argument is size_t.
+ *  - j   : argument is intmax_t.
+ *  - t   : argument is ptrdiff_t.
+ *
+ *  - d, i: signed decimal integer.
+ *  - u : unsigned decimal integer.
+ *  - x : unsigned hex
+ *  - p : pointer.
+ *  - s : null terminated str
+ *  - c : single char
+ *  - %
+ *
+ *  - b : binary representation
+ *  - I : ipv4 addr (expects uint8_t[4])
+ *  - M : mac addr (expects uint8_t[6])
+ */
+ void vprintfcb(putc_t putc, const char *fmt, va_list args);
