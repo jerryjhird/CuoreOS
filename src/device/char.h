568b7dff26784a682c2c2b5171b14140bfbb506e
jerryjhird <jerryjhird-public@proton.me>
2026-06-11 12:07:25 +0100

    logging: tidy log macro's to prevent compiler warnings

--- src/device/char.h
+++ src/device/char.h
@@ -9,15 +9,14 @@ void dev_puts(kernel_char_dev_t* dev, const char* s);
 void dev_printf(kernel_char_dev_t *dev, const char *fmt, ...);
 
 #ifdef DEBUG
-	#define dev_log_debug(dev, fmt, ...) dev_printf(dev, "(" ANSI_HI_BLACK " DEBUG " ANSI_RESET ") " fmt, ##__VA_ARGS__)
+	#define dev_log_debug(dev, fmt, ...) dev_printf(dev, "(" ANSI_HI_BLACK " DEBUG " ANSI_RESET ") " fmt __VA_OPT__(,) __VA_ARGS__)
 #else
 	#define dev_log_debug(dev, fmt, ...) ((void)0)
 #endif
 
-#define dev_log_ok(dev, fmt, ...) dev_printf(dev, "(" ANSI_BOLD_GREEN	"  OK   " ANSI_RESET ") ", ##__VA_ARGS__)
-#define dev_log_info(dev, fmt, ...) dev_printf(dev, "(" ANSI_REG_CYAN	" INFO  " ANSI_RESET ") ", ##__VA_ARGS__)
-#define dev_log_warn(dev, fmt, ...) dev_printf(dev, "(" ANSI_BOLD_YELLOW " WARN  " ANSI_RESET ") ", ##__VA_ARGS__)
-#define dev_log_error(dev, fmt, ...) dev_printf(dev, "(" ANSI_BOLD_RED   " ERROR " ANSI_RESET ") ", ##__VA_ARGS__)
-#define dev_log_critical(dev, fmt, ...) dev_printf(dev, "(" ANSI_BG_RED ANSI_BOLD_WHITE "	 CRIT	 " ANSI_RESET ") ", ##__VA_ARGS__)
-
+#define dev_log_ok(dev, fmt, ...) dev_printf(dev, "(" ANSI_BOLD_GREEN	"  OK   " ANSI_RESET ") " fmt __VA_OPT__(,) __VA_ARGS__)
+#define dev_log_info(dev, fmt, ...) dev_printf(dev, "(" ANSI_REG_CYAN	" INFO  " ANSI_RESET ") " fmt __VA_OPT__(,) __VA_ARGS__)
+#define dev_log_warn(dev, fmt, ...) dev_printf(dev, "(" ANSI_BOLD_YELLOW " WARN  " ANSI_RESET ") " fmt __VA_OPT__(,) __VA_ARGS__)
+#define dev_log_error(dev, fmt, ...) dev_printf(dev, "(" ANSI_BOLD_RED   " ERROR " ANSI_RESET ") " fmt __VA_OPT__(,) __VA_ARGS__)
+#define dev_log_critical(dev, fmt, ...) dev_printf(dev, "(" ANSI_BG_RED ANSI_BOLD_WHITE " CRIT  " ANSI_RESET ") " fmt __VA_OPT__(,) __VA_ARGS__)
 extern kernel_char_dev_t* debug_dev;
--- src/logbuf.h
+++ src/logbuf.h
@@ -22,13 +22,13 @@ void logbuf_flush(kernel_char_dev_t *target);
 void logbuf_clear(void);
 
 #ifdef DEBUG
-	#define logbuf_debug(fmt, ...) logbuf_printf("(" ANSI_HI_BLACK " DEBUG " ANSI_RESET ") " fmt, ##__VA_ARGS__)
+	#define logbuf_debug(fmt, ...) logbuf_printf("(" ANSI_HI_BLACK " DEBUG " ANSI_RESET ") " fmt __VA_OPT__(,) __VA_ARGS__)
 #else
 	#define logbuf_debug(fmt, ...) ((void)0)
 #endif
 
-#define logbuf_info(fmt, ...) logbuf_printf("(" ANSI_REG_CYAN	 " INFO  " ANSI_RESET ") " fmt, ##__VA_ARGS__)
-#define logbuf_ok(fmt, ...) logbuf_printf("(" ANSI_BOLD_GREEN	 "  OK   " ANSI_RESET ") " fmt, ##__VA_ARGS__)
-#define logbuf_warn(fmt, ...) logbuf_printf("(" ANSI_BOLD_YELLOW  " WARN  " ANSI_RESET ") " fmt, ##__VA_ARGS__)
-#define logbuf_error(fmt, ...) logbuf_printf("(" ANSI_BOLD_RED	" ERROR " ANSI_RESET ") " fmt, ##__VA_ARGS__)
-#define logbuf_critical(fmt, ...) logbuf_printf("(" ANSI_BG_RED ANSI_BOLD_WHITE " CRIT  " ANSI_RESET ") " fmt "", ##__VA_ARGS__)
+#define logbuf_info(fmt, ...) logbuf_printf("(" ANSI_REG_CYAN	 " INFO  " ANSI_RESET ") " fmt __VA_OPT__(,) __VA_ARGS__)
+#define logbuf_ok(fmt, ...) logbuf_printf("(" ANSI_BOLD_GREEN    "  OK   " ANSI_RESET ") " fmt __VA_OPT__(,) __VA_ARGS__)
+#define logbuf_warn(fmt, ...) logbuf_printf("(" ANSI_BOLD_YELLOW " WARN  " ANSI_RESET ") " fmt __VA_OPT__(,) __VA_ARGS__)
+#define logbuf_error(fmt, ...) logbuf_printf("(" ANSI_BOLD_RED	 " ERROR " ANSI_RESET ") " fmt __VA_OPT__(,) __VA_ARGS__)
+#define logbuf_critical(fmt, ...) logbuf_printf("(" ANSI_BG_RED ANSI_BOLD_WHITE " CRIT  " ANSI_RESET ") " fmt __VA_OPT__(,) __VA_ARGS__)
