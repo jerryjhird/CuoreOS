598cbad177f787ec6c6f56bc65a875f9e0f1166b
jerryjhird <jerryjhird-public@proton.me>
2026-06-12 16:28:26 +0100

    cmm: cmm drops its own memmap into a slice of a usable section of memory

--- src/mem/cmm.c
+++ src/mem/cmm.c
@@ -1,14 +1,15 @@
 #include "cmm.h"
 #include "kstate.h"
-#include <string.h>
+#include "panic.h"
 #include "logbuf.h"
+#include "mem/mem.h"
 
-#define MAX_CMM_REGIONS 64
-
-static struct cmm_region internal_map[MAX_CMM_REGIONS];
+static struct cmm_region *internal_map = NULL;
+static size_t internal_capacity = 0;
 static size_t internal_count = 0;
+
 static uintptr_t max_phys_addr = 0;
-uintptr_t max_ram_addr = 0;
+static uintptr_t max_ram_addr = 0;
 
 static cmm_type_t translate_type(uint64_t limine_type) {
 	switch (limine_type) {
@@ -26,31 +27,46 @@ static cmm_type_t translate_type(uint64_t limine_type) {
 }
 
 void cmm_init(void) {
-	struct limine_memmap_response *resp = memmap_request.response;
+    struct limine_memmap_response *resp = memmap_request.response;
 
-	for (size_t i = 0; i < resp->entry_count && i < MAX_CMM_REGIONS; i++) {
-		struct limine_memmap_entry *e = resp->entries[i];
+    size_t required_slots = resp->entry_count * 2;
+    size_t bytes_needed = (required_slots * sizeof(struct cmm_region) + 4095) & ~4095;
 
-		internal_map[i].base   = (uintptr_t)e->base;
-		internal_map[i].length = (size_t)e->length;
-		internal_map[i].type   = translate_type(e->type);
+	// find a usable region for the CMM memmap
+    for (size_t i = 0; i < resp->entry_count; i++) {
+        if (resp->entries[i]->type == LIMINE_MEMMAP_USABLE && resp->entries[i]->length >= bytes_needed) {
+            internal_map = (struct cmm_region*)((uintptr_t)resp->entries[i]->base + hhdm_offset);
+            internal_capacity = required_slots;
+            break;
+        }
+    }
 
-		internal_count++;
+    if (!internal_map) {
+        panic("CMM", "CRITICAL: Failed to locate memory for internal map!\n");
+    }
 
-		uintptr_t end = (uintptr_t)(e->base + e->length);
-		if (end > max_phys_addr) max_phys_addr = end;
-	}
+	// populate CMM memmap
+    for (size_t i = 0; i < resp->entry_count; i++) {
+        struct limine_memmap_entry *e = resp->entries[i];
+        uintptr_t e_base = (uintptr_t)e->base;
+        size_t e_len = (size_t)e->length;
 
-	for (size_t i = 0; i < internal_count; i++) {
-		if (internal_map[i].type == CMM_USABLE) {
-			uintptr_t end = internal_map[i].base + internal_map[i].length;
-			if (end > max_ram_addr) max_ram_addr = end;
-		}
-	}
+        if (e_base == (uintptr_t)internal_map - hhdm_offset) {
+            internal_map[internal_count++] = (struct cmm_region){e_base, bytes_needed, CMM_KERNEL_AND_MODULES};
+            internal_map[internal_count++] = (struct cmm_region){e_base + bytes_needed, e_len - bytes_needed, CMM_USABLE};
+        } else {
+            internal_map[internal_count++] = (struct cmm_region){e_base, e_len, translate_type(e->type)};
+        }
 
-	logbuf_ok("[ CMM  ] CMM initialized successfully\n");
-	logbuf_debug("[ CMM  ] Top of Physical address space: %p\n", (void*)max_phys_addr);
-	logbuf_debug("[ CMM  ] Top of RAM: %p\n", (void*)max_ram_addr);
+        uintptr_t end = internal_map[internal_count-1].base + internal_map[internal_count-1].length;
+        if (end > max_phys_addr) max_phys_addr = end;
+        if (internal_map[internal_count-1].type == CMM_USABLE && end > max_ram_addr) max_ram_addr = end;
+    }
+
+    logbuf_ok("[ CMM  ] CMM initialized successfully\n");
+    logbuf_debug("[ CMM  ] Total Regions: %zu\n", internal_count);
+    logbuf_debug("[ CMM  ] Top of Physical address space: %p\n", (void*)max_phys_addr);
+    logbuf_debug("[ CMM  ] Top of RAM: %p\n", (void*)max_ram_addr);
 }
 
 size_t cmm_get_region_count(void) { return internal_count; }
